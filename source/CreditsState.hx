c02153029d9af0272b35548f94bf1318f3a812db
Enzo824 <52637897+Enzo824@users.noreply.github.com>
2022-06-10 01:08:58 -0300

    a

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -65,7 +65,7 @@ class CreditsState extends MusicBeatState
 		#end
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'FFDD33'],
+			['Shadow Mo',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'FFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'C30085'],
 			['bb-panzu',			'bb-panzu',			'Additional Programmer of Psych Engine',			'https://twitter.com/bbsub3',			'389A58'],
 			[''],
